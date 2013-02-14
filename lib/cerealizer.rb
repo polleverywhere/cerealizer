9a14708e43c58e66b43723ff5892e0d43a69ca50
Brad Gessler <brad@polleverywhere.com>
2013-02-14 03:27:11 -0800

    href class attribute specs are passing, but the implemtation is fugly.

--- lib/cerealizer.rb
+++ lib/cerealizer.rb
@@ -2,58 +2,13 @@ require "cerealizer/version"
 require 'uri'
 require 'set'
 
-# A quick and dirty class for recognizing and generating URL paths.
-class PathRouter
-  PATH_DELIMITER = '/'
-  VARIABLE_REGEXP = /^\:(.+)/
-
-  attr_reader :uri
-
-  def initialize(uri)
-    @uri = URI.parse(uri)
-  end
-
-  # Return the segments out of this thing that are 
-  def recognize(uri)
-    if captures = regexp.match(URI.parse(uri).path).captures
-      Hash[variables.zip captures]
-    end
-  end
-
-  # Generate an URL based on a hash of variables
-  def generate(vars={})
-    segments.map{|s| s =~ VARIABLE_REGEXP ? vars[$1.to_sym] : s }.join(PATH_DELIMITER)
-  end
-
-  # A Set of the variables in the URL.
-  def variables
-    @variables ||= Set.new(segments.grep(VARIABLE_REGEXP){ $1 }.map(&:to_sym))
-  end
-
-private
-  # Convert the path into a Regexp that we can use to recognize shiz.
-  def regexp
-    @regexp ||= compile_regexp
-  end
-
-  # Get all of the segments in the URL.
-  def segments
-    @segments ||= uri.path.split(PATH_DELIMITER)
-  end
-
-  # Comples a regexp that's used to capture the URL and its segments.
-  def compile_regexp
-    Regexp.new("^#{segments.map{ |s| s =~ VARIABLE_REGEXP ? '(.+)' : s }.join('\/')}$")
-  end
-end
-
 module Cerealizer
   # Encapsulates information about keys.
   class Key
     attr_reader :name
 
     def initialize(name, &block)
-      @name = name
+      @name = name.to_sym
       self
     end
   end
@@ -64,6 +19,29 @@ module Cerealizer
 
     def initialize(object, scope)
       @object, @scope = object, scope
+
+      # Map out the hrefs. 
+      # TODO - This feels really messy. Should at least be implemented 
+      # as a sub-class to this or as class meta data that's resolved at runtime.
+      # TODO - Thise code asserts that all DB fk/pk have id at the end of them, which
+      # is too assumption. Also, if the word candid was used here, I'd assume that the
+      # 'id' at the end of candid is a db id and turn it into candhref. Not good!
+      self.class.class_eval do
+        hrefs.each do |path|
+          var = path.variables.first
+          href = var.to_s.gsub(/id$/, 'href')
+
+          define_method "#{href}" do
+            path.generate var => object.send(var)
+          end
+
+          define_method "#{href}=" do |href|
+            object.send "#{var}=", path.recognize(href)[var]
+          end
+        end
+      end
+      # Lets accept blocks, because, why not, right?
+      yield self if block_given?
       # K, back to your regularly scheduled programming...
       self
     end
@@ -98,6 +76,20 @@ module Cerealizer
       objects.map { |object| self.new(object,user).read_keys }
     end
 
+    def self.href(path)
+      path_router = PathRouter.new(path)
+      # TODO - Thise code asserts that all DB fk/pk have id at the end of them, which
+      # is too assumption. Also, if the word candid was used here, I'd assume that the
+      # 'id' at the end of candid is a db id and turn it into candhref. Not good!
+      key_name = path_router.variables.first.to_s.gsub(/id$/, 'href')
+      keys.push Key.new(key_name)
+      hrefs.push path_router
+    end
+
+    def self.hrefs
+      @hrefs ||= []
+    end
+
   private
     # TODO - Should the default be writable?
     def writeable?(key)
@@ -129,6 +121,51 @@ module Cerealizer
       else
         object.send(meth, *args)
       end
+    end
+  end
+
+  # A quick and dirty class for recognizing and generating URL paths.
+  class PathRouter
+    PATH_DELIMITER = '/'
+    VARIABLE_REGEXP = /^\:(.+)/
+
+    attr_reader :uri
+
+    def initialize(uri)
+      @uri = URI.parse(uri)
+    end
+
+    # Return the segments out of this thing that are 
+    def recognize(uri)
+      if captures = regexp.match(URI.parse(uri).path).captures
+        Hash[variables.zip captures]
+      end
+    end
+
+    # Generate an URL based on a hash of variables
+    def generate(vars={})
+      segments.map{|s| s =~ VARIABLE_REGEXP ? vars[$1.to_sym] : s }.join(PATH_DELIMITER)
+    end
+
+    # A Set of the variables in the URL.
+    def variables
+      @variables ||= Set.new(segments.grep(VARIABLE_REGEXP){ $1 }.map(&:to_sym))
+    end
+
+  private
+    # Convert the path into a Regexp that we can use to recognize shiz.
+    def regexp
+      @regexp ||= compile_regexp
+    end
+
+    # Get all of the segments in the URL.
+    def segments
+      @segments ||= uri.path.split(PATH_DELIMITER)
+    end
+
+    # Comples a regexp that's used to capture the URL and its segments.
+    def compile_regexp
+      Regexp.new("^#{segments.map{ |s| s =~ VARIABLE_REGEXP ? '(.+)' : s }.join('\/')}$")
     end
   end
 end
--- spec/lib/cerealizer_spec.rb
+++ spec/lib/cerealizer_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Cerealizer do
 
   # Anonomous classes we use to test cerealizer
-  let(:cereal_klass) { Struct.new(:name, :sugar_content, :private_notes, :owner_id, :prize) }
+  let(:cereal_klass) { Struct.new(:name, :sugar_content, :private_notes, :owner_id, :prize, :id) }
   let(:user_klass) { Struct.new(:name, :role, :id) }
   let(:serializer_klass) do
     # Weird eh? Its an anonymous class. Don't want to litter the Global namespace with
@@ -22,7 +22,7 @@ describe Cerealizer do
   # We're gonna use these instances for the test cases.
   let(:brad){ user_klass.new('Brad Gessler', :admin, 1) }
   let(:joe){ user_klass.new('Joe Shmoe', :anon, 2) }
-  let(:cheerios){ cereal_klass.new('Cheerios', '1 gram', 'Tastes like cardboard', brad.id) }
+  let(:cheerios){ cereal_klass.new('Cheerios', '1 gram', 'Tastes like cardboard', brad.id, 1) }
   let(:cheerios_serializer){ serializer_klass.new(cheerios, brad) }
 
   describe "#read_keys" do
@@ -53,7 +53,10 @@ describe Cerealizer do
   describe ".href" do
     let(:serializer_klass) do
       Class.new Cerealizer::Base do
+        key :id
+
         href "/users/:owner_id"
+        href "/fruits/:id"
       end
     end
 
@@ -62,13 +65,21 @@ describe Cerealizer do
     end
 
     it "should write #owner_href key" do
-      cheerios_serializer.write_keys(:owner_href => "/users/#{joe.id}").object.should == joe.id
+      cheerios_serializer.write_keys(:owner_href => "/users/#{joe.id}").object.owner_id.should == joe.id.to_s
+    end
+
+    it "should read #href key" do
+      cheerios_serializer.read_keys[:href].should == "/fruits/#{cheerios.id}"
+    end
+
+    it "should write #href key" do
+      cheerios_serializer.write_keys(:href => "/fruits/99").object.id.should == '99'
     end
   end
 end
 
-describe PathRouter do
-  let(:href) { PathRouter.new("/users/:owner_id") }
+describe Cerealizer::PathRouter do
+  let(:href) { Cerealizer::PathRouter.new("/users/:owner_id") }
 
   it "should recognize uri" do
     href.recognize("/users/12")[:owner_id].should == '12'
